a718cd8d1a3386b95ec70266143ed77eb5cc4c6d
Joao Macedo <joaovictormacedo91@gmail.com>
2023-08-03 16:48:01 -0300

    feat: add MuiButton and MuiTextField default styles

--- src/aktie-theme.ts
+++ src/aktie-theme.ts
@@ -1,15 +1,35 @@
 import { ThemeOptions } from '@mui/material/styles';
 
 export const AktieTheme: ThemeOptions = {
-  shape: {
-    borderRadius: 8
-  },
   palette: {
     primary: {
-      main: '#E1008B'
+      main: '#E1008B',
     },
     secondary: {
       main: '#8D1097'
+    },
+    error: {
+      main: '#F00000'
+    },
+  },
+  components: {
+    MuiButton: {
+      styleOverrides: {
+        root: {
+          borderRadius: 8,
+          boxShadow: 'none',
+          textTransform: 'capitalize',
+          ':focus': {
+            outline: '2px solid #0000008F',
+            outlineOffset: '3px',
+          }
+        }
+      }
+    },
+    MuiTextField: {
+      defaultProps: {
+        variant: 'outlined',
+      }
     }
   }
 };
