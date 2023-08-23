69a87236c8bcd21ef10726ffac25bbfeee7ef308
Joao Macedo <joaovictormacedo91@gmail.com>
2023-08-23 10:22:21 -0300

    feat: exporting new components

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,4 @@
 export { AktieTheme } from './aktie-theme';
 export { default as AktBackdrop } from './backdrop';
+export { default as AktErrorMessage } from './error-message';
+export { default as AktAutoCompleteVirtualized } from './auto-complete-virtualized';
